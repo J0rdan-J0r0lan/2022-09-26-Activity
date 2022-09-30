15e568f5f919b6f4e62b8a5b9649cce5826d9a3d
J0rdan-J0r0lan <112525580+J0rdan-J0r0lan@users.noreply.github.com>
2022-09-30 09:09:03 +0800

    Update hashmap.java
    edited file

--- hashmap.java
+++ hashmap.java
@@ -1,69 +1,76 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
-public class hashmap{
-        public static void main(String[] args) {
-            Map<Integer, String> hashMAP = new HashMap<Integer, String>();
-            Scanner s = new Scanner(System.in);
-            char myOption;
-            do{
-                System.out.println("Choose any from the following:");
-                System.out.println("");
-                System.out.println("=============================");
-                System.out.println("1. Add");
-                System.out.println("2. Remove");
-                System.out.println("3. Display");
-                System.out.println("4. Clear");
-                System.out.println("5. Exit");
-                System.out.println("=============================");
-                System.out.println("");
-                System.out.println("Enter your Choice:");
-                
-                myOption = s.next().charAt(0);
-                
 
-                switch(myOption){
-                    case'1':
-                        System.out.println("\nYou are going to ADD a NUMBER");
-                        for (int i = 0; i < 5; ++i) {
-                            System.out.println("Enter the number: ");
-                            Integer a = s.nextInt();
-                            String b = s.nextLine();
-                            if(a % 3 == 0){
-                                System.out.println("Youre not Allowed to add Prime numbers");
+class hashmap{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        
+        int option;
+        
+        do{
+
+             System.out.println("\n   **************************");
+            System.out.println(" \n  ");
+            System.out.println("   HASHMAP  ");
+            System.out.println("Choose any of the following below:  ");
+            System.out.print("\n   [1] Add Numbers \n   [2] Delete Numbers \n   [3] Display Numbers \n   [4] Clear \n   [5] Exit \n\n   Your Choice: ");
+            option = s.nextInt();
+            
+            switch(option){
+                case 1:
+                    for (int i = 0; i < 5; ++i) {
+                            System.out.print("   Enter 1st num: ");
+                            int number = s.nextInt();
+                            System.out.print("   Enter 2nd num: ");
+                            int c = s.nextInt();
+                            if(number % 3 == 0){
+                                System.out.println("   Youre not Allowed to add Prime numbers");
                             }else{
-                                hashMAP.put(a, b);
+                                hm.put(number, c);
+                                System.out.println("   "+number +" and "+c+" is added to the Hashmap.");  
                             }
                         }
-                        System.out.println("\n");
-                        break;
-                    case '2':
-                        System.out.println("\nYou are going to REMOVE a NUMBER");
-                        System.out.println("Enter the number to remove: ");
-                        Integer a = s.nextInt();
-                        hashMAP.remove(a);
-                        System.out.println("\n");
-                        break;
-                    case '3':
-                        System.out.println("\nYou Display HashMap Key Value");
-                        Set<Integer> keys = hashMAP.keySet();
-                        for(Integer key : keys){
-                            System.out.println("Key="+key + " Value="+hashMAP.get(key));
-                        }
-                        System.out.println("\n");
-                        break;
-                    case '4':
-                        System.out.println("\nYou Cleared The Hash Map Arrays Value!");
-                        hashMAP.clear();
-                        System.out.println("\n");
-                        break;
-                    case '5':
-                        System.out.println("\nThank You!, Goodbye!");
-                        System.exit(0);
-                        break;
-                }
-            }while(myOption != 1 || myOption != 2 || myOption != 3 || myOption != 4 || myOption != 5);
+                    
+                break;
+                
+                case 3:
+                    System.out.println("\n ");
+                    System.out.println("   Display Number ");
+                    System.out.println("   Map: " + hm);
+                    System.out.println("\n ");
+                    for (Map.Entry<Integer, Integer> m : hm.entrySet()) {
+                    System.out.println(m.getKey() + " " + m.getValue());
+        }
+                break;
+                case 2:
+                    System.out.print("   Choose number to delete: ");
+                    int num = s.nextInt();
+                    
+                    hm.remove(num);
+                    System.out.println("   The removed number number is:" + num);
+                    System.out.println("   Updated Map: " + hm);
+                    
+                    
+                break;
+                case 4:
+                    System.out.println("\n ");
+                    System.out.println("   All Numbers are Successfully Clear!!! ");
+                    hm.clear();
+                break;
+                case 5:
+                    
+                    System.out.println("   Thank You for Using!!! ");
+                    System.exit(0);
+                break;
+                
+                default:
+                System.err.println("   Invalid option selected");
+                System.exit(0);
+            }
+            
+            
+        }while(option != 0);
+            
     }
 }
